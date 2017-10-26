21403ab64ec05c2eca30ce7626ff3917ba1dee7b
Tuo <839539179@qq.com>
2017-10-26 15:23:36 +0800

    第一次提价代码，设计两个应用demo

--- app/src/main/java/com/example/tuo/auditprogressview/MainActivity.java
+++ app/src/main/java/com/example/tuo/auditprogressview/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.tuo.auditprogressview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+import com.example.tuo.auditprogressview.view.AuditProgressView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout content = findViewById(R.id.ll_audit_content);
+
+        content.addView(createView(5, true, true, true, false, "提交申请"));
+        content.addView(createView(5, true, true, false, false, "审查"));
+        content.addView(createView(5, true, true, false, false, "审核"));
+        content.addView(createView(5, true, false, false, false, "退款"));
+        content.addView(createView(5, false, false, false, false, "完成"));
+        content.addView(createView(5, false, false, false, true, "关闭"));
+    }
+
+
+    public AuditProgressView createView(int stepCount, boolean isCurrentComplete, boolean isNextComplete, boolean isFirstStep, boolean isLastStep, String text) {
+        AuditProgressView view = new AuditProgressView(this);
+        view.setStepCount(stepCount);
+        view.setIsCurrentComplete(isCurrentComplete);
+        view.setIsNextComplete(isNextComplete);
+        view.setIsFirstStep(isFirstStep);
+        view.setIsLastStep(isLastStep);
+        view.setText(text);
+        return view;
     }
 }
